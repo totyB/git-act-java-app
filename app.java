9ca70188a6e4b7a6ed6c9efe6a288ae79e186c49
maharoof <mahafoosraheem123@gmail.com>
2026-07-24 16:16:58 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static String getMessage() {
+        return "Hello from GitHub Actions!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getMessage());
+    }
+}
